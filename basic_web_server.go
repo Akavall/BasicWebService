bd23416bd7c4fff91e86ff61ddcf53d228432b27
Kirill <Akavall@gmail.com>
2018-08-18 19:09:57 -0700

    parsing html template

--- basic_web_server.go
+++ basic_web_server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 
 	"google.golang.org/appengine"
 )
@@ -11,7 +13,15 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 	// We are not using r, but we need it
 	// because HandleFunc requires it
 
-	fmt.Fprintf(w, "Hey, Hey Again")
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
 
 func main() {
